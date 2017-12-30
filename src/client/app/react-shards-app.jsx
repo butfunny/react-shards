c0a6d9657f6d9802c7d359460fa8d241962fe228
CuongNguyen <p1code@prototype1.io>
2017-12-30 10:40:11 +0700

    added modal

--- src/client/app/react-shards-app.jsx
+++ src/client/app/react-shards-app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Slider} from "./slider/single/slider";
 import {RangeSlider} from "./slider/multiple/range-slider";
+import {modals, ModalsRegistry} from "./modal/modals";
 
 export class ReactShardsApp extends React.Component {
 
@@ -13,6 +14,36 @@ export class ReactShardsApp extends React.Component {
                 to: 5
             }
         };
+
+    }
+
+    openModal() {
+        let modal = modals.openModal({
+            content: (
+                <div className="modal-test">
+                    <div className="modal-header">
+                        <div className="modal-title">
+                            Are you sure?
+                        </div>
+                    </div>
+
+                    <div className="modal-body">
+                        Et has debitis vivendo, nam dicant malorum te. Justo moderatius elaboraret cu est, nibh placerat insolens id mea. Suas equidem usu ad, eos ex sint stet alterum. Sit in adhuc propriae contentiones, dicta decore eum an.
+                    </div>
+
+                    <div className="modal-footer">
+                        <button className="btn btn-secondary">
+                            Nope
+                        </button>
+
+                        <button className="btn btn-primary">
+                            Yep
+                        </button>
+                    </div>
+                </div>
+            )
+        })
+
     }
 
     render() {
@@ -29,6 +60,10 @@ export class ReactShardsApp extends React.Component {
                     onChange={(value) => this.setState({value})}
                 />
 
+                <button
+                    className="btn btn-primary"
+                    onClick={() => this.openModal()}>Click me</button>
+
                 {/*<RangeSlider*/}
                     {/*tooltip*/}
                     {/*max={10}*/}
@@ -36,6 +71,8 @@ export class ReactShardsApp extends React.Component {
                     {/*value={multipleValue}*/}
                     {/*onChange={(multipleValue) => this.setState({multipleValue})}*/}
                 {/*/>*/}
+
+                <ModalsRegistry />
             </div>
         );
     }
